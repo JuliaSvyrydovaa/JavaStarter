496fcbc45d6d03860d0549fe806b19a027435263
JuliaSvyrydovaa <julj39549@gmail.com>
2019-10-01 14:58:17 +0300

    The program is on the branch dev!

---
+++ src/Tasks.java
@@ -0,0 +1,10 @@
+public class Tasks {
+        public static void main(String[] args)
+        {
+            String firsttask="1.Create a new repository and name it JavaStarter";
+            String secondtask="2.Create branch dev";
+            String thirdtask="3.Create a program that will output word JAVA to the console";
+            System.out.println("You have to do this task tommorow:");
+            System.out.println(thirdtask);
+        }
+}
